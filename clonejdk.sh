6800e078994bf1cfd51dbfce8329e4d75bcc9ae2
Duy Tran Khanh <40482367+khanhduytran0@users.noreply.github.com>
2022-08-23 07:28:28 +0700

    Exclude src/java.desktop/macosx

--- clonejdk.sh
+++ clonejdk.sh
@@ -6,4 +6,6 @@ if [ "$BUILD_IOS" != "1" ]; then
   git clone --depth 1 https://github.com/PojavLauncherTeam/mobile openjdk
 else
   git clone --depth 1 https://github.com/PojavLauncherTeam/jdk17u openjdk
+  # Hack: exclude building macOS stuff
+  mv openjdk/src/java.desktop/macosx openjdk/src/java.desktop/macosx_NOTIOS
 fi
