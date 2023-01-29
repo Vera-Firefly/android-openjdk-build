e83f84e80f4c8e3d316dc8890ec804b03d1c44bb
Duy Tran Khanh <40482367+khanhduytran0@users.noreply.github.com>
2023-01-30 06:58:17 +0700

    Fix: include libjsound natives

--- clonejdk.sh
+++ clonejdk.sh
@@ -7,5 +7,8 @@ if [ "$BUILD_IOS" != "1" ]; then
 else
   git clone --depth 1 https://github.com/PojavLauncherTeam/jdk17u openjdk
   # Hack: exclude building macOS stuff
-  mv openjdk/src/java.desktop/macosx openjdk/src/java.desktop/macosx_NOTIOS
+  desktop_mac=openjdk/src/java.desktop/macos
+  mv ${desktop_mac} ${desktop_mac}_NOTIOS
+  mkdir -p ${desktop_mac}/native
+  mv ${desktop_mac}_NOTIOS/native/libjsound ${desktop_mac}/native/
 fi
