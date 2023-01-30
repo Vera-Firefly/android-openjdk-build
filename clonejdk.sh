838021dd082f4bdbcedf0d3375f1b875a0a5bab9
Duy Tran Khanh <40482367+khanhduytran0@users.noreply.github.com>
2023-01-30 00:58:02 +0000

    Fix(iOS build): who stole the x

--- clonejdk.sh
+++ clonejdk.sh
@@ -7,7 +7,7 @@ if [ "$BUILD_IOS" != "1" ]; then
 else
   git clone --depth 1 https://github.com/PojavLauncherTeam/jdk17u openjdk
   # Hack: exclude building macOS stuff
-  desktop_mac=openjdk/src/java.desktop/macos
+  desktop_mac=openjdk/src/java.desktop/macosx
   mv ${desktop_mac} ${desktop_mac}_NOTIOS
   mkdir -p ${desktop_mac}/native
   mv ${desktop_mac}_NOTIOS/native/libjsound ${desktop_mac}/native/
