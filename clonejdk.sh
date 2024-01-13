3d1fd8b550ef2731feb3eb925cc74f5a0ad1ad6c
aaaapai <89151134+aaaapai@users.noreply.github.com>
2024-01-13 18:13:16 +0800

    Update clonejdk.sh

--- clonejdk.sh
+++ clonejdk.sh
@@ -7,5 +7,5 @@ git clone --depth 1 --branch ios https://github.com/PojavLauncherTeam/openjdk-mu
 else
 # Use aarch32 repo because it also has aarch64
 
-git clone --depth 1 https://github.com/openjdk/jdk8u openjdk
+git clone -b shenandoah-jdk8u402-b05 --depth 1 https://github.com/gnu-andrew/shenandoah-jdk8u openjdk
 fi
