2d0ad05b91f396e83113f358034a39747afe0510
aaaapai <89151134+aaaapai@users.noreply.github.com>
2024-01-13 12:11:47 +0800

    Update ci_build_global.sh

--- ci_build_global.sh
+++ ci_build_global.sh
@@ -5,8 +5,8 @@ set -e
 export JDK_DEBUG_LEVEL=release
 
 if [ "$BUILD_IOS" != "1" ]; then
-  sudo apt-get update
-  sudo apt-get -y install autoconf python-is-python3 unzip zip
+  sudo apt update
+  sudo apt -y install autoconf python3 unzip zip
 
   wget -nc -nv -O android-ndk-$NDK_VERSION-linux-x86_64.zip "https://dl.google.com/android/repository/android-ndk-$NDK_VERSION-linux-x86_64.zip"
   ./extractndk.sh
