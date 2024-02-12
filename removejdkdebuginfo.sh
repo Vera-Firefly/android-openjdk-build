f8ebd4b6ddbd7f61b389c85dfa5da1db2133086a
aaaapai <89151134+aaaapai@users.noreply.github.com>
2024-02-12 16:53:16 +0800

    Update removejdkdebuginfo.sh

--- removejdkdebuginfo.sh
+++ removejdkdebuginfo.sh
@@ -26,10 +26,7 @@ if [[ "$TARGET_JDK" == "aarch64" ]] || [[ "$TARGET_JDK" == "x86_64" ]]; then
    echo "Building for aarch64 or x86_64, introducing JVMCI module"
    export EXTRA_JLINK_OPTION=,jdk.internal.vm.ci
 fi
-
-export EXTRA_JLINK_OPTION=
-
-   echo "introducing JVMCI module"
+   echo "jdk incubator vector"
    export EXTRA_JLINK_OPTION=,jdk.incubator.vector
 
 # Produce the jre equivalent from the jdk (https://blog.adoptium.net/2021/10/jlink-to-produce-own-runtime/)
