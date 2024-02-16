678c60ff68a49916faee178ebe748f404698f201
giaobupai <137373766+giaobupai@users.noreply.github.com>
2024-02-16 16:09:46 +0800

    Update ci_build_global.sh

--- ci_build_global.sh
+++ ci_build_global.sh
@@ -4,7 +4,7 @@ set -e
 
 export JDK_DEBUG_LEVEL=release
 
-if [ "$BUILD_IOS" != "1" ]; then
+if [[ "$BUILD_IOS" != "1" ]]; then
 
   if [[ -z "$NDK_USE_EXISTING" ]]; then
     wget -nc -nv -O android-ndk-$NDK_VERSION-linux-x86_64.zip "https://dl.google.com/android/repository/android-ndk-$NDK_VERSION-linux-x86_64.zip"
