3f28a819f7ce48f56c2a4938aae2f56f6bd2bb0f
giaobupai <137373766+giaobupai@users.noreply.github.com>
2024-02-16 16:07:50 +0800

    Update buildjdk.sh

--- buildjdk.sh
+++ buildjdk.sh
@@ -5,14 +5,14 @@ set -e
 export FREETYPE_DIR=$PWD/freetype-$BUILD_FREETYPE_VERSION/build_android-$TARGET_SHORT
 export CUPS_DIR=$PWD/cups-2.2.4
 export CFLAGS+=" -DLE_STANDALONE" # -I$FREETYPE_DIR -I$CUPS_DI
-if [ "$TARGET_JDK" == "arm" ] # || [ "$BUILD_IOS" == "1" ]
+if [[ "$TARGET_JDK" == "arm" ]] # || [[ "$BUILD_IOS" == "1" ]]
 then
   export CFLAGS+=" -O3 -D__thumb__"
 else
   export CFLAGS+=" -O3"
 fi
 
-# if [ "$TARGET_JDK" == "aarch32" ] || [ "$TARGET_JDK" == "aarch64" ]
+# if [[ "$TARGET_JDK" == "aarch32" ]] || [[ "$TARGET_JDK" == "aarch64" ]]
 # then
 #   export CFLAGS+=" -march=armv7-a+neon"
 # fi
@@ -23,7 +23,7 @@ fi
 # cp -R /usr/include/X11 $ANDROID_INCLUDE/
 # cp -R /usr/include/fontconfig $ANDROID_INCLUDE/
 
-if [ "$BUILD_IOS" != "1" ]; then
+if [[ "$BUILD_IOS" != "1" ]]; then
   chmod +x android-wrapped-clang
   chmod +x android-wrapped-clang++
   ln -s -f /usr/include/X11 $ANDROID_INCLUDE/
@@ -31,6 +31,7 @@ if [ "$BUILD_IOS" != "1" ]; then
   platform_args="--with-toolchain-type=gcc \
     --with-freetype-include=$FREETYPE_DIR/include/freetype2 \
     --with-freetype-lib=$FREETYPE_DIR/lib \
+    --build=x86_64-unknown-linux-gnu \
     "
   AUTOCONF_x11arg="--x-includes=$ANDROID_INCLUDE/X11"
   AUTOCONF_EXTRA_ARGS+="OBJCOPY=$OBJCOPY \
@@ -50,7 +51,7 @@ else
   ln -s -f /opt/X11/include/X11 $ANDROID_INCLUDE/
   ln -sfn $themacsysroot/System/Library/Frameworks/CoreAudio.framework/Headers $ANDROID_INCLUDE/CoreAudio
   ln -sfn $themacsysroot/System/Library/Frameworks/IOKit.framework/Headers $ANDROID_INCLUDE/IOKit
-  if [ "$(uname -p)" == "arm" ]; then
+  if [[ "$(uname -p)" == "arm" ]]; then
     ln -s -f /opt/homebrew/include/fontconfig $ANDROID_INCLUDE/
   else
     ln -s -f /usr/local/include/fontconfig $ANDROID_INCLUDE/
@@ -76,7 +77,7 @@ cd openjdk
 
 # Apply patches
 git reset --hard
-if [ "$BUILD_IOS" != "1" ]; then
+if [[ "$BUILD_IOS" != "1" ]]; then
   git apply --reject --whitespace=fix ../patches/jdk21u_android.diff || echo "git apply failed (Android patch set)"
 else
   git apply --reject --whitespace=fix ../patches/jdk21u_ios.diff || echo "git apply failed (iOS patch set)"
@@ -113,7 +114,7 @@ bash ./configure \
     --x-libraries=/usr/lib \
         $platform_args || \
 error_code=$?
-if [ "$error_code" -ne 0 ]; then
+if [[ "$error_code" -ne 0 ]]; then
   echo "\n\nCONFIGURE ERROR $error_code , config.log:"
   cat config.log
   exit $error_code
@@ -121,14 +122,14 @@ fi
 
 jobs=4
 
-if [ "$BUILD_IOS" == "1" ]; then
+if [[ "$BUILD_IOS" == "1" ]]; then
   jobs=$(sysctl -n hw.ncpu)
 fi
 
 cd build/${JVM_PLATFORM}-${TARGET_JDK}-${JVM_VARIANTS}-${JDK_DEBUG_LEVEL}
 make JOBS=$jobs images || \
 error_code=$?
-if [ "$error_code" -ne 0 ]; then
+if [[ "$error_code" -ne 0 ]]; then
   echo "Build failure, exited with code $error_code. Trying again."
   make JOBS=$jobs images
 fi
