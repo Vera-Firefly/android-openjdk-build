eaacbe74e19c5d2dcb2f058061bf14d206d10fbe
aaaapai <89151134+aaaapai@users.noreply.github.com>
2024-02-24 01:10:04 +0800

    Update clonejdk.sh

--- clonejdk.sh
+++ clonejdk.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 set -euo pipefail
-if [[ "$TARGET_JDK" == "arm" ]]; then
-git clone --depth 1 https://github.com/openjdk/aarch32-port-jdk8u openjdk
-elif [ "$BUILD_IOS" == "1" ]; then
-git clone --depth 1 --branch ios https://github.com/PojavLauncherTeam/openjdk-multiarch-jdk8u openjdk
-else
-# Use aarch32 repo because it also has aarch64
 
 git clone -b revert-621-dev --depth 1 https://github.com/dragonwell-project/dragonwell8 openjdk
-fi
