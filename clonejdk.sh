4c2c9f858ed5218f2824dbc49c2bfd614181318b
aaaapai <89151134+aaaapai@users.noreply.github.com>
2024-02-24 01:15:20 +0800

    Update clonejdk.sh

--- clonejdk.sh
+++ clonejdk.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -euo pipefail
 
-git clone -b revert-621-dev --depth 1 https://github.com/dragonwell-project/dragonwell8 openjdk
+git clone --depth 1 https://github.com/dragonwell-project/dragonwell8 openjdk
