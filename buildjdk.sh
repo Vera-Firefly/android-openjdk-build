0348ea789021f24e6d185618cd4c26a23e4eb453
Acxy-fu <140683097+Acxy-fu@users.noreply.github.com>
2024-02-29 19:24:53 +0800

    Update buildjdk.sh

--- buildjdk.sh
+++ buildjdk.sh
@@ -57,11 +57,7 @@ cd openjdk
 
 # Apply patches
 git reset --hard
-if [[ "$TARGET_JDK" == "arm" ]] || [[ "$TARGET_JDK" == "x86" ]]; then
-  git apply --reject --whitespace=fix ../patches/jdk21u_android_32.diff || echo "git apply failed (Android patch set)"
-else
-  git apply --reject --whitespace=fix ../patches/jdk21u_android_64.diff || echo "git apply failed (Android patch set)"
-fi
+git apply --reject --whitespace=fix ../patches/jdk21u_android.diff || echo "git apply failed (Android patch set)"
 
 # rm -rf build
 
