dde3c36eeff5db9006c83a1f1b1b74840e1ac2af
aaaapai <89151134+aaaapai@users.noreply.github.com>
2024-03-02 22:05:41 +0800

    Update removejdkdebuginfo.sh

--- removejdkdebuginfo.sh
+++ removejdkdebuginfo.sh
@@ -18,10 +18,15 @@ cp -r $targetpath/images/jdk jdkout
 #cp -r jdkout/{conf,legal,lib,man,release} jreout/
 #rm jreout/lib/src.zip
 
+if [[ "$TARGET_JDK" == "aarch64" ]] || [[ "$TARGET_JDK" == "x86_64" ]]; then
+   echo "Building for aarch64 or x86_64, introducing JVMCI module"
+   export EXTRA_JLINK_OPTION=,jdk.internal.vm.ci
+fi
+
 # Produce the jre equivalent from the jdk (https://blog.adoptium.net/2021/10/jlink-to-produce-own-runtime/)
 $targetpath/buildjdk/jdk/bin/jlink \
 --module-path=jdkout/jmods \
---add-modules java.base,java.compiler,java.datatransfer,java.desktop,java.instrument,java.logging,java.management,java.management.rmi,java.naming,java.net.http,java.prefs,java.rmi,java.scripting,java.se,java.security.jgss,java.security.sasl,java.sql,java.sql.rowset,java.transaction.xa,java.xml,java.xml.crypto,jdk.accessibility,jdk.charsets,jdk.crypto.cryptoki,jdk.crypto.ec,jdk.dynalink,jdk.httpserver,jdk.jdwp.agent,jdk.jfr,jdk.jsobject,jdk.localedata,jdk.management,jdk.management.agent,jdk.management.jfr,jdk.naming.dns,jdk.naming.rmi,jdk.net,jdk.nio.mapmode,jdk.sctp,jdk.security.auth,jdk.security.jgss,jdk.unsupported,jdk.xml.dom,jdk.zipfs \
+--add-modules java.base,java.compiler,java.datatransfer,java.desktop,java.instrument,java.logging,java.management,java.management.rmi,java.naming,java.net.http,java.prefs,java.rmi,java.scripting,java.se,java.security.jgss,java.security.sasl,java.sql,java.sql.rowset,java.transaction.xa,java.xml,java.xml.crypto,jdk.accessibility,jdk.charsets,jdk.crypto.cryptoki,jdk.crypto.ec,jdk.dynalink,jdk.httpserver,jdk.jdwp.agent,jdk.jfr,jdk.jsobject,jdk.localedata,jdk.management,jdk.management.agent,jdk.management.jfr,jdk.naming.dns,jdk.naming.rmi,jdk.net,jdk.nio.mapmode,jdk.sctp,jdk.security.auth,jdk.security.jgss,jdk.unsupported,jdk.xml.dom,jdk.zipfs,jdk.incubator.foreign,jdk.incubator.vector$EXTRA_JLINK_OPTION \
 --output jreout \
 --strip-debug \
 --no-man-pages \
