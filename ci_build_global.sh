5fa65972261b6a72907fb7d060c2de6e4d3d3f93
aaaapai <89151134+aaaapai@users.noreply.github.com>
2024-03-02 21:58:38 +0800

    Update ci_build_global.sh

--- ci_build_global.sh
+++ ci_build_global.sh
@@ -4,9 +4,6 @@ set -e
 
 export JDK_DEBUG_LEVEL=release
 
-sudo apt-get update
-sudo apt-get -y install autoconf python-is-python3 unzip zip
-
 if [[ -z "$NDK_USE_EXISTING" ]]; then
   wget -nc -nv -O android-ndk-$NDK_VERSION-linux-x86_64.zip "https://dl.google.com/android/repository/android-ndk-$NDK_VERSION-linux-x86_64.zip"
   ./extractndk.sh
@@ -14,7 +11,6 @@ fi
 
 # Some modifies to NDK to fix
 
-./getbootjdk.sh
 ./getlibs.sh
 ./buildlibs.sh
 ./clonejdk.sh
