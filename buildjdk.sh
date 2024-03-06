07a8674f2654970e69b012c64a1a357dfc73ec71
aaaapai <89151134+aaaapai@users.noreply.github.com>
2024-03-06 12:21:56 +0800

    Update buildjdk.sh

--- buildjdk.sh
+++ buildjdk.sh
@@ -59,9 +59,9 @@ cd openjdk
 # Apply patches
 git reset --hard
 if [[ "$TARGET_JDK" == "arm" ]] || [[ "$TARGET_JDK" == "x86" ]]; then
-  git apply --reject --whitespace=fix ../patches/jdk21u_android_32.diff || echo "git apply failed (Android patch set)"
+  git apply --reject --whitespace=fix ../patches/jdk17u_android_32.diff || echo "git apply failed (Android patch set)"
 else
-  git apply --reject --whitespace=fix ../patches/jdk21u_android_64.diff || echo "git apply failed (Android patch set)"
+  git apply --reject --whitespace=fix ../patches/jdk17u_android_64.diff || echo "git apply failed (Android patch set)"
 fi
 
 # rm -rf build
@@ -70,6 +70,7 @@ fi
 #   --with-extra-cflags="$CPPFLAGS" \
 
 bash ./configure \
+    --with-version-pre=- \
     --openjdk-target=$TARGET \
     --with-extra-cflags="$CFLAGS" \
     --with-extra-cxxflags="$CFLAGS" \
