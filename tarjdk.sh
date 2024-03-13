eb62a128b0d93caf2adeb9422258909ccfadf799
aaaapai <89151134+aaaapai@users.noreply.github.com>
2024-03-14 00:04:01 +0800

    Add files via upload

--- tarjdk.sh
+++ tarjdk.sh
@@ -24,11 +24,12 @@ findexec jreout | xargs -- ./termux-elf-cleaner/termux-elf-cleaner
 findexec jdkout | xargs -- ./termux-elf-cleaner/termux-elf-cleaner
 
 cp -rv jre_override/lib/* jreout/lib/ || true
+cp -rv jre_override/lib/* jdkout/lib/ || true
 
 cd jreout
 
 # Strip in place all .so files thanks to the ndk
-find ./ -name '*.so' -execdir $NDK/toolchains/llvm/prebuilt/linux-x86_64/${NDK_PREBUILT_ARCH}-linux-android/bin/strip {} \;
+find ./ -name '*.so' -execdir $NDK/toolchains/llvm/prebuilt/linux-x86_64/bin/llvm-strip {} \;
 
 tar cJf ../jre17-${TARGET_SHORT}-`date +%Y%m%d`-${JDK_DEBUG_LEVEL}.tar.xz .
 
