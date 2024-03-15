b2b3d330dbef7f84b40173d64c0cd6763ff2d69f
aaaapai <89151134+aaaapai@users.noreply.github.com>
2024-03-15 23:25:00 +0800

    Update clonejdk.sh

--- clonejdk.sh
+++ clonejdk.sh
@@ -2,10 +2,6 @@
 set -e
 if [[ "$TARGET_JDK" == "arm" ]]; then
 git clone --depth 1 https://github.com/openjdk/aarch32-port-jdk8u openjdk
-elif [ "$BUILD_IOS" == "1" ]; then
-git clone --depth 1 --branch ios https://github.com/PojavLauncherTeam/openjdk-multiarch-jdk8u openjdk
 else
-# Use aarch32 repo because it also has aarch64
-
-git clone --depth 1 https://github.com/openjdk/jdk8u openjdk
+git clone --depth 1 https://github.com/adoptium/jdk8u openjdk
 fi
