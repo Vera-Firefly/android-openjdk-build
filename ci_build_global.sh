fdd9ac06e98274407a93ce64eeecd2d6723d2fdc
aaaapai <89151134+aaaapai@users.noreply.github.com>
2024-03-19 12:53:09 +0800

    Update ci_build_global.sh

--- ci_build_global.sh
+++ ci_build_global.sh
@@ -7,6 +7,7 @@ export JDK_DEBUG_LEVEL=release
 if [[ -z "$NDK_USE_EXISTING" ]]; then
   wget -nc -nv -O android-ndk-$NDK_VERSION-linux-x86_64.zip "https://dl.google.com/android/repository/android-ndk-$NDK_VERSION-linux-x86_64.zip"
   ./extractndk.sh
+  ./maketoolchain.sh
 fi
 
 # Some modifies to NDK to fix
