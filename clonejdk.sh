295c7bb46ed7d76dcf633f20160e6d656b5d8148
Vera-Firefly <87926662+Vera-Firefly@users.noreply.github.com>
2024-03-25 09:13:54 +0800

    Update clonejdk.sh

--- clonejdk.sh
+++ clonejdk.sh
@@ -3,5 +3,5 @@ set -e
 if [[ "$TARGET_JDK" == "arm" ]]; then
 git clone --depth 1 https://github.com/openjdk/aarch32-port-jdk8u openjdk
 else
-git clone --depth 1 https://github.com/adoptium/jdk8u openjdk
+git clone --depth 1 https://github.com/openjdk/jdk8u openjdk
 fi
