ca23998f633553ff1cd5fc01dabdc80287b10a7f
aaaapai <89151134+aaaapai@users.noreply.github.com>
2024-03-29 17:40:09 +0800

    Update ci_build_global.sh

--- ci_build_global.sh
+++ ci_build_global.sh
@@ -5,6 +5,7 @@ set -e
 export JDK_DEBUG_LEVEL=release
 
 chmod 777 maketoolchain.sh
+chmod 777 extractndk.sh
 
 wget -nc -nv -O android-ndk-$NDK_VERSION-linux.zip "https://dl.google.com/android/repository/android-ndk-$NDK_VERSION-linux.zip"
 ./extractndk.sh
@@ -17,6 +18,7 @@ chmod 777 buildlibs.sh
 chmod 777 buildjdk.sh
 chmod 777 removejdkdebuginfo.sh
 chmod 777 tarjdk.sh
+chomd 777 clonejdk.sh
 
 ./getlibs.sh
 ./buildlibs.sh
