cbdd19c9213d7bcdfba087cad53164194f99064b
aaaapai <89151134+aaaapai@users.noreply.github.com>
2024-06-22 23:14:46 +0800

    Update clonejdk.sh

--- clonejdk.sh
+++ clonejdk.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-git clone --branch jdk21.0.1 --depth 1 https://github.com/openjdk/jdk21u openjdk-21
+git clone --branch jdk21.0.1 --depth 1 https://github.com/openjdk/jdk21u openjdk
