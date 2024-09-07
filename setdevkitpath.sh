8b552704bf733377e585d6803f4248476a6e74dd
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2024-09-07 16:37:56 +0800

    Update setdevkitpath.sh

--- setdevkitpath.sh
+++ setdevkitpath.sh
@@ -41,8 +41,14 @@ export ANDROID_INCLUDE=$TOOLCHAIN/sysroot/usr/include
 export CPPFLAGS="-I$ANDROID_INCLUDE -I$ANDROID_INCLUDE/$TARGET" # -I/usr/include -I/usr/lib
 export LDFLAGS="-L$TOOLCHAIN/sysroot/usr/lib/${TARGET}/${API}"
 
-export thecc=$TOOLCHAIN/bin/${TARGET}${API}-clang
-export thecxx=$TOOLCHAIN/bin/${TARGET}${API}-clang++
+if [[ "$TARGET_JDK" == "arm" ]]
+then
+  export thecc=$TOOLCHAIN/bin/${TARGET_1}${API}-clang
+  export thecxx=$TOOLCHAIN/bin/${TARGET_1}${API}-clang++
+else
+  export thecc=$TOOLCHAIN/bin/${TARGET}${API}-clang
+  export thecxx=$TOOLCHAIN/bin/${TARGET}${API}-clang++
+fi
 
 # Configure and build.
 export DLLTOOL=/usr/bin/llvm-dlltool-18
