2ded5ebb8ced876b5a2e6c0609e8daeb5f064ea7
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2025-03-07 13:07:50 +0800

    Update buildlibs.sh

--- buildlibs.sh
+++ buildlibs.sh
@@ -22,5 +22,5 @@ if [[ "$error_code" -ne 0 ]]; then
   exit $error_code
 fi
 
-CFLAGS="-O3 -fno-emulated-tls -fno-rtti -march=armv8-a+simd+crc+crypto+fp16" CXXFLAGS="-Ofast -fno-emulated-tls -fno-rtti -march=armv8-a+simd+crc+crypto+fp16" make -j4
+CFLAGS="-O3 -fno-emulated-tls -fno-rtti -march=armv8-a+simd" CXXFLAGS="-Ofast -fno-emulated-tls -fno-rtti -march=armv8-a+simd" make -j4
 make install
