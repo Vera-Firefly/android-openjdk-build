aaaa60ff8c4607ff6cef452d58af8e8952a09c31
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2025-03-15 13:19:59 +0800

    Update buildlibs.sh

--- buildlibs.sh
+++ buildlibs.sh
@@ -9,7 +9,7 @@ export PATH=$TOOLCHAIN/bin:$PATH
 ./configure \
   --host=$TARGET \
   --prefix=${PWD}/build_android-${TARGET_SHORT} \
-  LD=$TOOLCHAIN/bin/ld.lld \
+  LD=$TOOLCHAIN/bin/ld \
   --without-zlib \
   --with-brotli=system \
   --with-png=no \
