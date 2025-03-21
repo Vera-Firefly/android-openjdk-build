2f7eee653af22eb3eda68623de1f1dca39a9a136
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2025-03-22 00:54:25 +0800

    Update tarjdk.sh

--- tarjdk.sh
+++ tarjdk.sh
@@ -2,29 +2,29 @@
 set -e
 . setdevkitpath.sh
 
-unset AR AS CC CXX LD OBJCOPY RANLIB STRIP CPPFLAGS LDFLAGS
-git clone --depth 1 https://github.com/termux/termux-elf-cleaner || true
-cd termux-elf-cleaner
-mkdir build
-cd build
-export CFLAGS=-D__ANDROID_API__=24
-cmake ..
-make -j4
-unset CFLAGS
-cd ../..
+# unset AR AS CC CXX LD OBJCOPY RANLIB STRIP CPPFLAGS LDFLAGS
+# git clone --depth 1 https://github.com/termux/termux-elf-cleaner || true
+# cd termux-elf-cleaner
+# mkdir build
+# cd build
+# export CFLAGS=-D__ANDROID_API__=24
+# cmake ..
+# make -j4
+# unset CFLAGS
+# cd ../..
 
-findexec() { find $1 -type f -name "*" -not -name "*.o" -exec sh -c '
-    case "$(head -n 1 "$1")" in
-      ?ELF*) exit 0;;
-      MZ*) exit 0;;
+# findexec() { find $1 -type f -name "*" -not -name "*.o" -exec sh -c '
+#     case "$(head -n 1 "$1")" in
+#       ?ELF*) exit 0;;
+#       MZ*) exit 0;;
       #!*/ocamlrun*)exit0;;
-    esac
-exit 1
-' sh {} \; -print
-}
+#     esac
+# exit 1
+# ' sh {} \; -print
+# }
 
-findexec jreout | xargs -- ./termux-elf-cleaner/build/termux-elf-cleaner
-findexec jdkout | xargs -- ./termux-elf-cleaner/build/termux-elf-cleaner
+# findexec jreout | xargs -- ./termux-elf-cleaner/build/termux-elf-cleaner
+# findexec jdkout | xargs -- ./termux-elf-cleaner/build/termux-elf-cleaner
 
 cp -rv jre_override/lib/* jreout/lib/ || true
 cp -rv jre_override/lib/* jdkout/lib/ || true
