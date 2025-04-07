4392c54703e1f5c6931215dde9124ecd3456bb06
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2025-04-07 18:28:32 +0800

    Update tarjdk.sh

--- tarjdk.sh
+++ tarjdk.sh
@@ -23,8 +23,8 @@ exit 1
 ' sh {} \; -print
 }
 
-findexec jreout | xargs --api-level 24 ./termux-elf-cleaner/build/termux-elf-cleaner
-findexec jdkout | xargs --api-level 24 ./termux-elf-cleaner/build/termux-elf-cleaner
+findexec jreout | xargs ./termux-elf-cleaner/build/termux-elf-cleaner --api-level 24
+findexec jdkout | xargs ./termux-elf-cleaner/build/termux-elf-cleaner --api-level 24
 
 cp -rv jre_override/lib/* jreout/lib/ || true
 cp -rv jre_override/lib/* jdkout/lib/ || true
