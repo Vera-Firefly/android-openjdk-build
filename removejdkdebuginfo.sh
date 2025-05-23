cd00547f50ccb64081eee8241f550eaaba0fd612
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2025-05-23 21:57:07 +0800

    Update removejdkdebuginfo.sh

--- removejdkdebuginfo.sh
+++ removejdkdebuginfo.sh
@@ -41,6 +41,8 @@ $JLINK_STRIP_ARG \
 
 cp freetype-$BUILD_FREETYPE_VERSION/build_android-$TARGET_SHORT/lib/libfreetype.so jreout/lib/
 cp freetype-$BUILD_FREETYPE_VERSION/build_android-$TARGET_SHORT/lib/libfreetype.so jdkout/lib/
+cp awt_xawt/libawt_xawt.so jreout/lib/
+cp awt_xawt/libawt_xawt.so jdkout/lib/
 
 # mv jreout/lib/${TARGET_JDK}/libfontmanager.diz jreout/lib/${TARGET_JDK}/libfontmanager.diz.keep
 # find jreout -name "*.debuginfo" | xargs -- rm
