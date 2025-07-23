832cc525ead87361e161855590e124cd5e02f3d9
aaaapai <89151134+aaaapai@users.noreply.github.com>
2025-07-23 15:21:22 +0800

    Update repackjre.sh

--- repackjre.sh
+++ repackjre.sh
@@ -13,9 +13,16 @@ work="$in/work"
 work1="$in/work1"
 
 # make sure paths exist
-mkdir -p $work
-mkdir -p $work1
+mkdir -p "$work"
+mkdir -p "$work1"
 mkdir -p "$out"
+
+copyjvmlib() {
+  if [[ -d lib/$1 ]]; then
+    echo "Moving $1 VM for $2"
+    mv lib/$1 "$work1"/lib/;
+  fi
+}
 
 # here comes a not-so-complicated functions to easily make desired arch
 ## Usage: makearch [jre_libs_dir_name] [name_in_tarball]
@@ -28,9 +35,11 @@ makearch () {
   
   #mv lib/$1 "$work1"/lib/;
   mv lib/jexec "$work1"/lib/;
+  mv lib/jvm.cfg "$work1"/lib/;
   
   # server contains the libjvm.so
-  mv lib/server "$work1"/lib/;
+  copyjvmlib server $2
+  copyjvmlib client $2
   
   # All the other .so files are at the root of the lib folder
   find ./ -name '*.so' -execdir mv {} "$work1"/lib/{} \;
@@ -52,6 +61,7 @@ makeuni () {
   rm -rf bin;
   rm -rf lib/server;
   rm lib/jexec;
+  rm lib/jvm.cfg;
   find ./ -name '*.so' -execdir rm {} \; # Remove arch specific shared objects
   rm release
   
@@ -67,4 +77,10 @@ makearch aarch64 arm64
 makearch i386 x86
 makearch amd64 x86_64
 
+# if running under GitHub Actions, write commit sha, else formatted system date
+if [[ -n "$GITHUB_SHA" ]]
+then
+echo $GITHUB_SHA>"$out"/version
+else
 date +%Y%m%d>"$out"/version
+fi
