6f97ba59cbe57aaff0731e64cc140377d9800ea8
aaaapai <89151134+aaaapai@users.noreply.github.com>
2025-07-23 15:23:59 +0800

    Update setdevkitpath.sh

--- setdevkitpath.sh
+++ setdevkitpath.sh
@@ -1,10 +1,10 @@
 # Use the old NDK r10e to not get internal compile error at (still?)
 # https://github.com/PojavLauncherTeam/openjdk-multiarch-jdk8u/blob/aarch64-shenandoah-jdk8u272-b10/jdk/src/share/native/sun/java2d/loops/GraphicsPrimitiveMgr.c
-export NDK_VERSION=r21
+export NDK_VERSION=r28
 
 if [[ -z "$BUILD_FREETYPE_VERSION" ]]
 then
-  export BUILD_FREETYPE_VERSION="2.13.2"
+  export BUILD_FREETYPE_VERSION="2.13.3"
 fi
 
 if [[ -z "$JDK_DEBUG_LEVEL" ]]
@@ -26,26 +26,34 @@ fi
 
 export JVM_PLATFORM=linux
 # Set NDK
-export API=21
-export NDK=$PWD/android-ndk-$NDK_VERSION
-export ANDROID_NDK_ROOT=$NDK
-export TOOLCHAIN=$NDK/generated-toolchains/android-${TARGET_SHORT}-toolchain
-# export TOOLCHAIN=$NDK/toolchains/llvm/prebuilt/linux-x86_64
+export API=24
 
+# Runners usually ship with a recent NDK already
+if [[ -z "$ANDROID_NDK_LATEST_HOME" ]]
+then
+  export ANDROID_NDK_HOME=$PWD/android-ndk-$NDK_VERSION
+fi
+
+export TOOLCHAIN=$ANDROID_NDK_LATEST_HOME/toolchains/llvm/prebuilt/linux-x86_64
 export ANDROID_INCLUDE=$TOOLCHAIN/sysroot/usr/include
 
 export CPPFLAGS="-I$ANDROID_INCLUDE -I$ANDROID_INCLUDE/$TARGET" # -I/usr/include -I/usr/lib
-export LDFLAGS="-L$NDK/platforms/android-$API/arch-$TARGET_SHORT/usr/lib"
-
-export thecc=$TOOLCHAIN/bin/$TARGET-gcc
-export thecxx=$TOOLCHAIN/bin/$TARGET-g++
+export LDFLAGS="-fuse-ld=lld"
+export thecc=$TOOLCHAIN/bin/${TARGET}${API}-clang
+export thecxx=$TOOLCHAIN/bin/${TARGET}${API}-clang++
 
 # Configure and build.
-export AR=$TOOLCHAIN/bin/$TARGET-ar
-export AS=$TOOLCHAIN/bin/$TARGET-as
-export CC=$PWD/android-wrapped-clang
-export CXX=$PWD/android-wrapped-clang++
-export LD=$TOOLCHAIN/bin/$TARGET-ld
-export OBJCOPY=$TOOLCHAIN/bin/$TARGET-objcopy
-export RANLIB=$TOOLCHAIN/bin/$TARGET-ranlib
-export STRIP=$TOOLCHAIN/bin/$TARGET-strip
+export DLLTOOL=$TOOLCHAIN/bin/llvm-dlltool
+export CXXFILT=$TOOLCHAIN/bin/llvm-cxxfilt
+export LD=$TOOLCHAIN/bin/ld
+export NM=$TOOLCHAIN/bin/llvm-nm
+export CC=$thecc
+export CXX=$thecxx
+export AR=$TOOLCHAIN/bin/llvm-ar
+export AS=$TOOLCHAIN/bin/llvm-as
+export OBJCOPY=$TOOLCHAIN/bin/llvm-objcopy
+export OBJDUMP=$TOOLCHAIN/bin/llvm-objdump
+export READELF=$TOOLCHAIN/bin/llvm-readelf
+export RANLIB=$TOOLCHAIN/bin/llvm-ranlib
+export STRIP=$TOOLCHAIN/bin/llvm-strip
+export LINK=$TOOLCHAIN/bin/llvm-link
