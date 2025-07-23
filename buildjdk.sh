89fba968122f933addf7c26d6576058c948e8243
aaaapai <89151134+aaaapai@users.noreply.github.com>
2025-07-23 17:00:39 +0800

    Update buildjdk.sh

--- buildjdk.sh
+++ buildjdk.sh
@@ -101,7 +101,7 @@ bash ./configure \
     --enable-option-checking=fatal \
     --enable-headless-only=yes \
     --with-jvm-variants=$JVM_VARIANTS \
-    --with-jvm-features=-dtrace,-zero,-vm-structs,-epsilongc,link-time-opt,opt-size \
+    --with-jvm-features=-dtrace,-zero,-vm-structs,-epsilongc \
     --with-cups-include=$CUPS_DIR \
     --with-devkit=$TOOLCHAIN \
     --with-native-debug-symbols=external \
