7104e64791f34fce2f5b7b81974dcd6b72c2d673
aaaapai <89151134+aaaapai@users.noreply.github.com>
2025-07-23 15:26:32 +0800

    Update tarjdk.sh

--- tarjdk.sh
+++ tarjdk.sh
@@ -3,12 +3,15 @@ set -e
 . setdevkitpath.sh
 
 unset AR AS CC CXX LD OBJCOPY RANLIB STRIP CPPFLAGS LDFLAGS
-git clone --depth 1 https://github.com/ShirosakiMio/termux-elf-cleaner || true
+git clone --depth 1 https://github.com/termux/termux-elf-cleaner || true
 cd termux-elf-cleaner
-autoreconf --install
-bash configure
-make CFLAGS=-D__ANDROID_API__=24
-cd ..
+mkdir build
+cd build
+export CFLAGS=-D__ANDROID_API__=${API}
+cmake ..
+make -j4
+unset CFLAGS
+cd ../..
 
 findexec() { find $1 -type f -name "*" -not -name "*.o" -exec sh -c '
     case "$(head -n 1 "$1")" in
@@ -20,19 +23,26 @@ exit 1
 ' sh {} \; -print
 }
 
-findexec jreout | xargs -- ./termux-elf-cleaner/termux-elf-cleaner
-findexec jdkout | xargs -- ./termux-elf-cleaner/termux-elf-cleaner
+findexec jreout | xargs ./termux-elf-cleaner/build/termux-elf-cleaner --api-level ${API}
+findexec jdkout | xargs ./termux-elf-cleaner/build/termux-elf-cleaner --api-level ${API}
 
 cp -rv jre_override/lib/* jreout/lib/ || true
 cp -rv jre_override/lib/* jdkout/lib/ || true
 
+if [ "${TARGET_SHORT}" = "arm64" ] && [ -f jreout/lib/jspawnhelper ]; then
+    cp jreout/lib/jspawnhelper libjsph11.so
+fi
+
 cd jreout
 
 # Strip in place all .so files thanks to the ndk
-find ./ -name '*.so' -execdir $NDK/toolchains/llvm/prebuilt/linux-x86_64/${NDK_PREBUILT_ARCH}-linux-android/bin/strip {} \;
+find ./ -name '*.so' -execdir ${TOOLCHAIN}/bin/llvm-strip {} \;
 
 tar cJf ../jre11-${TARGET_SHORT}-`date +%Y%m%d`-${JDK_DEBUG_LEVEL}.tar.xz .
 
 cd ../jdkout
 tar cJf ../jdk11-${TARGET_SHORT}-`date +%Y%m%d`-${JDK_DEBUG_LEVEL}.tar.xz .
 
+# Remove jreout and jdkout
+cd ..
+rm -rf jdkout jreout
